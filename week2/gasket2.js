10d49eef4507c9dcc73b27494ca70d6e9e0c5d87
CM Lubinski <cm.lubinski@gmail.com>
2015-07-14 20:24:34 -0400

    Move code into an App namespace

--- week2/gasket2.js
+++ week2/gasket2.js
@@ -1,106 +1,73 @@
 
-var canvas;
-var gl;
-var program;
-
-var points = [];
-
-var NumTimesToSubdivide = 5;
-
-window.onload = function init()
-{
-    canvas = document.getElementById( "gl-canvas" );
+var App = {
+  points: [],
+  init: function() {
+    canvas = document.getElementById("gl-canvas");
+    this.gl = WebGLUtils.setupWebGL(canvas);
+    if (!this.gl) { alert( "WebGL isn't available" ); }
     
-    gl = WebGLUtils.setupWebGL( canvas );
-    if ( !gl ) { alert( "WebGL isn't available" ); }
-        
-    //
-    //  Initialize our data for the Sierpinski Gasket
-    //
-
-    // First, initialize the corners of our gasket with three points.
-    
-    var vertices = [
-        vec2( -1, -1 ),
-        vec2(  0,  1 ),
-        vec2(  1, -1 )
-    ];
-
-    divideTriangle( vertices[0], vertices[1], vertices[2],
-                    NumTimesToSubdivide);
-
-    //
     //  Configure WebGL
-    //
-    gl.viewport( 0, 0, canvas.width, canvas.height );
-    gl.clearColor( 1.0, 1.0, 1.0, 1.0 );
+    this.gl.viewport(0, 0, canvas.width, canvas.height);
+    this.gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
     //  Load shaders and initialize attribute buffers
-    
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-
-    // Load the data into the GPU
-    
-    var bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-
-    // Associate out shader variables with our data buffer
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
-
-    var theta = gl.getUniformLocation(program, "theta");
-    gl.uniform1f(theta, 0.0);
-
-    var dFactor = gl.getUniformLocation(program, "dFactor");
-    gl.uniform1f(dFactor, 1.0);
-
-    render();
-    $('#theta').knob({
-      "release": function(value) {
-        gl.uniform1f(theta, value * Math.PI / -50);
-        render();
-      }
-    });
-};
-
-function triangle( a, b, c )
-{
-    points.push( a, b, c );
-}
-
-function divideTriangle( a, b, c, count )
-{
-
-    // check for end of recursion
-    
-    if ( count === 0 ) {
-        triangle( a, b, c );
-    }
-    else {
-    
+    this.program = initShaders(this.gl, "vertex-shader", "fragment-shader");
+    this.gl.useProgram(this.program);
+  },
+  sendPoints: function() {
+    var bufferId = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, bufferId);
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, flatten(this.points),
+                       this.gl.STATIC_DRAW );
+  },
+  setTheta: function(value) {
+    var theta = this.gl.getUniformLocation(this.program, "theta");
+    this.gl.uniform1f(theta, value);
+  },
+  setVPosition: function() {
+    var vPosition = this.gl.getAttribLocation(this.program, "vPosition");
+    this.gl.vertexAttribPointer(vPosition, 2, this.gl.FLOAT, false, 0, 0);
+    this.gl.enableVertexAttribArray(vPosition);
+  },
+  recalcVertices: function(numDivisions) {
+    this.points = [];
+    this.divideTriangle(vec2(-1, -1), vec2(0, 1), vec2(1, -1), numDivisions);
+  },
+  divideTriangle: function(a, b, c, count) {
+    // base case
+    if (count === 0) {
+      this.points.push(a, b, c);
+    } else {
         //bisect the sides
-        
-        var ab = mix( a, b, 0.5 );
-        var ac = mix( a, c, 0.5 );
-        var bc = mix( b, c, 0.5 );
+        var ab = mix(a, b, 0.5);
+        var ac = mix(a, c, 0.5);
+        var bc = mix(b, c, 0.5);
 
         --count;
 
         // three new triangles
-        
-        divideTriangle( a, ab, ac, count );
-        divideTriangle( c, ac, bc, count );
-        divideTriangle( b, bc, ab, count );
+        this.divideTriangle(a, ab, ac, count);
+        this.divideTriangle(c, ac, bc, count);
+        this.divideTriangle(b, bc, ab, count);
     }
+  },
+  render: function() {
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    this.gl.drawArrays(this.gl.TRIANGLES, 0, this.points.length);
+  }
 }
 
-function render()
-{
-    gl.clear( gl.COLOR_BUFFER_BIT );
-    gl.drawArrays( gl.TRIANGLES, 0, points.length );
-}
-
+$(function() {
+  App.init();
+  App.recalcVertices(5);
+  App.sendPoints();
+  App.setVPosition();
+  App.setTheta(0.0);
+  App.render();
+  $('#theta').knob({
+    release: function(value) {
+      App.setTheta(value * Math.PI / -50);
+      App.render();
+    }
+  });
+});
