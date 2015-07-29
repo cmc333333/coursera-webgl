a10543ceee8f2ec5a15b02e53a874d725a67a668
CM Lubinski <cm.lubinski@gmail.com>
2015-07-29 01:21:24 -0400

    Add cylindar. Center the objects

--- assignment3/cad.js
+++ assignment3/cad.js
@@ -1,14 +1,12 @@
 var Cone = function(numBasePoints) {
-  var verts = [vec3(0, 0.5, 0)],
+  var verts = [vec3(0, 0.25, 0)],
       wireframe = [];
 
   for (var i = 0; i < numBasePoints; i++) {
     var theta = 2 * Math.PI * i / numBasePoints;
-    verts.push(vec3(Math.cos(theta)/4, 0, Math.sin(theta)/4));
-    wireframe.push(0);
-    wireframe.push(i+1);
-    wireframe.push(i+1);
-    wireframe.push(i+2);
+    verts.push(vec3(Math.cos(theta)/4, -0.25, Math.sin(theta)/4));
+    wireframe.push(0, i+1);   // line from tip to here
+    wireframe.push(i+1, i+2); // line from here to next point
   }
   wireframe[wireframe.length - 1] = 1;    // link the last bit of the circle
 
@@ -16,6 +14,28 @@ var Cone = function(numBasePoints) {
     vertices: verts,
     mode: "TRIANGLE_FAN",
     indices: _.range(numBasePoints + 1),
+    wireframe: wireframe
+  };
+};
+
+var Cylinder = function(numBasePoints) {
+  var verts = [],
+      wireframe = [];
+
+  for (var i = 0; i < numBasePoints; i++) {
+    var theta = 2 * Math.PI * i / numBasePoints;
+    verts.push(vec3(Math.cos(theta)/4, -0.25, Math.sin(theta)/4));
+    verts.push(vec3(Math.cos(theta)/4, 0.25, Math.sin(theta)/4));
+    wireframe.push(2*i, 2*i+1);                     // vertical
+    wireframe.push(2*i, (2*i+2) % (numBasePoints*2));   // horizontal (bottom)
+    wireframe.push(2*i, (2*i+3) % (numBasePoints*2));   // diagonal
+    wireframe.push(2*i+1, (2*i+3) % (numBasePoints*2)); // horizontal (top)
+  }
+
+  return {
+    vertices: verts,
+    mode: "TRIANGLE_STRIP",
+    indices: [],
     wireframe: wireframe
   };
 };
@@ -42,22 +62,26 @@ var App = {
     this.gl.vertexAttribPointer(vPosition, 3, this.gl.FLOAT, false, 0, 0);
     this.gl.enableVertexAttribArray(vPosition);
   },
-  cone: Cone(6),
+  cone: Cone(12),
+  cylinder: Cylinder(12),
+  editing: null,
   render: function() {
     var transformLoc = this.gl.getUniformLocation(this.program, "transform"),
         transform = mult(rotate(this.rotateX(), 1, 0, 0),
                     mult(rotate(this.rotateY(), 0, 1, 0),
                          rotate(this.rotateZ(), 0, 0, 1)));
+    this.editing = this.cylinder;
+
     this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 
     this.gl.uniformMatrix4fv(transformLoc, false, flatten(transform));
-    this.gl.bufferData(this.gl.ARRAY_BUFFER, flatten(this.cone.vertices),
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, flatten(this.editing.vertices),
                        this.gl.STATIC_DRAW);
     
-    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(this.cone.wireframe),
+    this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(this.editing.wireframe),
                        this.gl.STATIC_DRAW);
 
-    this.gl.drawElements(this.gl.LINES, this.cone.wireframe.length, this.gl.UNSIGNED_BYTE, 0);
+    this.gl.drawElements(this.gl.LINES, this.editing.wireframe.length, this.gl.UNSIGNED_BYTE, 0);
   }
 }
 
