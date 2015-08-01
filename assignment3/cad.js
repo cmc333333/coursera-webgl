25daca40c513d043a9c59ad0ec1d659c94c0fd37
CM Lubinski <cm.lubinski@gmail.com>
2015-07-31 21:40:31 -0400

    Color selection

--- assignment3/cad.js
+++ assignment3/cad.js
@@ -26,12 +26,14 @@ var App = {
   editing: null,
   render: function() {
     var transformLoc = this.gl.getUniformLocation(this.program, "transform"),
+        color = this.gl.getUniformLocation(this.program, "color");
         transform = mult(translate(this.positionX(), this.positionY(), this.positionZ()),
                     mult(rotate(this.rotateX(), 1, 0, 0),
                     mult(rotate(this.rotateY(), 0, 1, 0),
                     mult(rotate(this.rotateZ(), 0, 0, 1),
                          scale(this.skewX(), this.skewY(), this.skewZ())))));
     this.editing = this.sphere;
+    this.gl.uniform4f(color, this.color()[0], this.color()[1], this.color()[2], 1);
 
     this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 
@@ -68,47 +70,41 @@ var App = {
 }
 
 $(function() {
-  var $rotateX = $('#rotate_x'),
-      $rotateY = $('#rotate_y'),
-      $rotateZ = $('#rotate_z'),
-      $positionX = $('#position_x'),
-      $positionY = $('#position_y'),
-      $positionZ = $('#position_z'),
+  var valOf = function(search) { return function() { return $(search).val(); }},
       $scale = $('#scale'),
-      $skewX = $('#skew_x'),
-      $skewY = $('#skew_y'),
-      $skewZ = $('#skew_z'),
-      $wireframe = $('input[type=checkbox]');
+      $wireframe = $('input[type=checkbox]'),
+      $colors = $('input.color');
 
   App.initGL();
-  App.rotateX = function() { return $rotateX.val(); }
-  App.rotateY = function() { return $rotateY.val(); }
-  App.rotateZ = function() { return $rotateZ.val(); }
+  App.rotateX = valOf('#rotate_x');
+  App.rotateY = valOf('#rotate_y');
+  App.rotateZ = valOf('#rotate_z');
+  App.positionX = valOf('#position_x');
+  App.positionY = valOf('#position_y');
+  App.positionZ = valOf('#position_z');
+  App.skewX = valOf('#skew_x');
+  App.skewY = valOf('#skew_y');
+  App.skewZ = valOf('#skew_z');
   App.wireframe = function() { return $wireframe.is(':checked'); }
-  App.positionX = function() { return $positionX.val(); }
-  App.positionY = function() { return $positionY.val(); }
-  App.positionZ = function() { return $positionZ.val(); }
-  App.skewX = function() { return $skewX.val(); }
-  App.skewY = function() { return $skewY.val(); }
-  App.skewZ = function() { return $skewZ.val(); }
+  App.color = function() {
+    var $selected = $colors.filter('.selected'),
+        colorStr = $selected.css('background-color').slice(4, -1),
+        components = colorStr.split(', ');
+    return $.map(components, function(v) { return parseInt(v)/255; });
+  };
   App.render();
 
-  $rotateX.on("input change", App.render.bind(App));
-  $rotateY.on("input change", App.render.bind(App));
-  $rotateZ.on("input change", App.render.bind(App));
-  $wireframe.on("input change", App.render.bind(App));
-  $positionX.on("input change", App.render.bind(App));
-  $positionY.on("input change", App.render.bind(App));
-  $positionZ.on("input change", App.render.bind(App));
-  $skewX.on("input change", App.render.bind(App));
-  $skewY.on("input change", App.render.bind(App));
-  $skewZ.on("input change", App.render.bind(App));
+  $('input[type=range]').not($scale).on("input change", App.render.bind(App));
   $scale.on("input change", function() {
     var val = $scale.val();
-    $skewX.val(val);
-    $skewY.val(val);
-    $skewZ.val(val);
+    $('[data-tab-body=scale] input').val(val);
     App.render();
   });
+  $wireframe.click(App.render.bind(App));
   $('.tab a').click(App.onTabClick.bind(App)).first().click();
+  $colors.click(function() {
+    $colors.removeClass('selected');
+    $(this).addClass('selected');
+    App.render();
+  });
 });
